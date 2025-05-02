732790c6b1a6ccec1b8bbab7f920274a9d4801d8
natcurti <naticurti01@gmail.com>
2025-05-02 19:38:26 -0300

    Create table Musica and class MusicaDAL

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -2,25 +2,23 @@
 using ScreenSound.Menus;
 using ScreenSound.Modelos;
 
+//ScreenSoundContext context = new ScreenSoundContext();
+//ArtistaDAL artistaDAL = new ArtistaDAL(context);
+
 try
 {
-    var context = new ScreenSoundContext();
-    var artistaDAL = new ArtistaDAL(context);
+    ScreenSoundContext context = new ScreenSoundContext();
+    MusicaDAL musicaDAL = new MusicaDAL(context);
 
-    Artista novoArtista = new Artista("Scorpions", "Scorpions é uma banda muito legal e interessante") { Id = 1002};
+    musicaDAL.AdicionarMusica(new Musica("Como Yo Nadie Te Ha Amado"));
+    musicaDAL.AdicionarMusica(new Musica("Never Say Goodbye"));
+    var listaMusicas = musicaDAL.Listar();
 
-    //artistaDAL.AdicionarArtista(novoArtista);
-    //artistaDAL.AtualizarArtista(novoArtista);
-    //artistaDAL.DeletarArtista(novoArtista);
+    foreach(var musica in listaMusicas)
+    {
+        Console.WriteLine(musica.ToString());
+    }
 
-    //var listaArtistas = artistaDAL.Listar();
-    //foreach (var artist in listaArtistas)
-    //{
-    //    Console.WriteLine(artist);
-    //}
-
-    Artista artistaBuscado = artistaDAL.RecuperarPeloNome("Bon Jovi")!;
-    Console.WriteLine(artistaBuscado!.ToString());
 }
 catch (Exception ex)
 {
@@ -29,57 +27,50 @@ catch (Exception ex)
 
 return;
 
-Artista ira = new Artista("Ira!", "Banda Ira!");
-Artista beatles = new("The Beatles", "Banda The Beatles");
+//Dictionary<int, Menu> opcoes = new();
+//opcoes.Add(1, new MenuRegistrarArtista());
+//opcoes.Add(2, new MenuRegistrarMusica());
+//opcoes.Add(3, new MenuMostrarArtistas());
+//opcoes.Add(4, new MenuMostrarMusicas());
+//opcoes.Add(-1, new MenuSair());
 
-Dictionary<string, Artista> artistasRegistrados = new();
-artistasRegistrados.Add(ira.Nome, ira);
-artistasRegistrados.Add(beatles.Nome, beatles);
+//void ExibirLogo()
+//{
+//    Console.WriteLine(@"
 
-Dictionary<int, Menu> opcoes = new();
-opcoes.Add(1, new MenuRegistrarArtista());
-opcoes.Add(2, new MenuRegistrarMusica());
-opcoes.Add(3, new MenuMostrarArtistas());
-opcoes.Add(4, new MenuMostrarMusicas());
-opcoes.Add(-1, new MenuSair());
+//░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+//██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+//╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+//░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+//██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+//╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+//");
+//    Console.WriteLine("Boas vindas ao Screen Sound 3.0!");
+//}
 
-void ExibirLogo()
-{
-    Console.WriteLine(@"
+//void ExibirOpcoesDoMenu()
+//{
+//    ExibirLogo();
+//    Console.WriteLine("\nDigite 1 para registrar um artista");
+//    Console.WriteLine("Digite 2 para registrar a música de um artista");
+//    Console.WriteLine("Digite 3 para mostrar todos os artistas");
+//    Console.WriteLine("Digite 4 para exibir todas as músicas de um artista");
+//    Console.WriteLine("Digite -1 para sair");
 
-░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
-██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
-╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
-░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
-██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
-╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
-");
-    Console.WriteLine("Boas vindas ao Screen Sound 3.0!");
-}
+//    Console.Write("\nDigite a sua opção: ");
+//    string opcaoEscolhida = Console.ReadLine()!;
+//    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
 
-void ExibirOpcoesDoMenu()
-{
-    ExibirLogo();
-    Console.WriteLine("\nDigite 1 para registrar um artista");
-    Console.WriteLine("Digite 2 para registrar a música de um artista");
-    Console.WriteLine("Digite 3 para mostrar todos os artistas");
-    Console.WriteLine("Digite 4 para exibir todas as músicas de um artista");
-    Console.WriteLine("Digite -1 para sair");
+//    if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
+//    {
+//        Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
+//        menuASerExibido.Executar(artistaDAL);
+//        if (opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
+//    } 
+//    else
+//    {
+//        Console.WriteLine("Opção inválida");
+//    }
+//}
 
-    Console.Write("\nDigite a sua opção: ");
-    string opcaoEscolhida = Console.ReadLine()!;
-    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
-
-    if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
-    {
-        Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
-        menuASerExibido.Executar(artistasRegistrados);
-        if (opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
-    } 
-    else
-    {
-        Console.WriteLine("Opção inválida");
-    }
-}
-
-ExibirOpcoesDoMenu();
+//ExibirOpcoesDoMenu();
